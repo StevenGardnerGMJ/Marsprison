717cb02bb9707f62fc66c2ed313c07b908156d16
zkk <1970226903@qq.com>
2019-07-30 16:24:26 +0800

    开始故事

---
+++ Mars/Mars/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mars
+//
+//  Created by boco on 2019/7/30.
+//  Copyright © 2019 尘埃三笠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+// 火星监狱
+/*
+ 如何开始这个故事，这是个问题
+ 
+ */
+
+
